b546c37f84b747f83c5016d7f8d5a9e2f362e397
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-07-17 19:22:19 +0300

    create value manager.

--- lib/presentation/font_manager.dart
+++ lib/presentation/font_manager.dart
@@ -12,11 +12,3 @@ class FontWeightManager {
   static const FontWeight bold = FontWeight.w700;
 }
 
-class FontSize {
-  static const double s12 = 12.0;
-  static const double s14 = 14.0;
-  static const double s16 = 16.0;
-  static const double s18 = 18.0;
-  static const double s20 = 20.0;
-  static const double s22 = 22.0;
-}
