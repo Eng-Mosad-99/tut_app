256d5452f1b4ede7bd044cfa588d80abd15d30ff
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-07-22 13:45:47 +0300

    onboarding view part 1

--- lib/presentation/onboarding/on_boarding_view.dart
+++ lib/presentation/onboarding/on_boarding_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tut_app_mvvm_clean_arch/presentation/resources/string_manager.dart';
 
+import '../resources/assets_manager.dart';
 import '../resources/color_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
@@ -10,11 +13,63 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderData();
+  final PageController _pageController = PageController();
+  int _currentIndex = 0;
+
+  List<SliderObject> _getSliderData() => [
+        SliderObject(
+            title: AppStrings.onBoardingTitle1,
+            subTitle: AppStrings.onBoardingSubTitle1,
+            image: ImageAssets.onboardingLogo1),
+        SliderObject(
+            title: AppStrings.onBoardingTitle2,
+            subTitle: AppStrings.onBoardingSubTitle2,
+            image: ImageAssets.onboardingLogo2),
+        SliderObject(
+            title: AppStrings.onBoardingTitle3,
+            subTitle: AppStrings.onBoardingSubTitle3,
+            image: ImageAssets.onboardingLogo3),
+        SliderObject(
+            title: AppStrings.onBoardingTitle4,
+            subTitle: AppStrings.onBoardingSubTitle4,
+            image: ImageAssets.onboardingLogo4),
+      ];
+
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-      backgroundColor: ColorManager.grey2,
-      body: const Center(child: Text("Welcome to on boarding")),
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarBrightness: Brightness.dark,
+        ),
+      ),
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: _list.length,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        itemBuilder: (context, index) {
+          // return onboarding page
+        },
+      ),
     );
   }
+}
+
+class SliderObject {
+  final String title;
+  final String subTitle;
+  final String image;
+
+  SliderObject({
+    required this.title,
+    required this.subTitle,
+    required this.image,
+  });
 }
